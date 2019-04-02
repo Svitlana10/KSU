a7aec3af14133531c08d95c42830d47b9293668c
ComradeYanis <wot2304@gmail.com>
2019-04-03 00:42:13 +0300

    update main page filter

--- models/searchs/ArticleSearch.php
+++ models/searchs/ArticleSearch.php
@@ -51,7 +51,7 @@ class ArticleSearch extends Article
             'pagination' => ['pageSize' => $pages]
         ]);
 
-        $this->load($params);
+        $this->load($params, '');
 
         if (!$this->validate()) {
             // uncomment the following line if you do not want to return any records when validation fails
