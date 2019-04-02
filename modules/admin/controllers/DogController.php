a6f1d0e0bc3888c5dec2b331311b725bba19f891
ComradeYanis <wot2304@gmail.com>
2019-04-03 00:23:40 +0300

    add document writer

--- modules/admin/controllers/DogController.php
+++ modules/admin/controllers/DogController.php
@@ -2,15 +2,21 @@
 
 namespace app\modules\admin\controllers;
 
+use app\models\Dog;
 use app\models\DogBreeds;
 use app\models\searchs\DogSearch;
+use PhpOffice\PhpWord\Exception\Exception;
+use PhpOffice\PhpWord\IOFactory;
+use PhpOffice\PhpWord\PhpWord;
 use Throwable;
 use Yii;
-use app\models\Dog;
 use yii\db\StaleObjectException;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+use yii\helpers\FileHelper;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
-use yii\filters\VerbFilter;
+use yii\web\Response;
 
 /**
  * DogController implements the CRUD actions for Dog model.
@@ -29,6 +35,17 @@ class DogController extends Controller
                     'delete' => ['POST'],
                 ],
             ],
+            'access' => [
+                'class' => AccessControl::class,
+                'only' => ['index', 'create', 'update', 'view', 'delete', 'document'],
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'actions' => ['index', 'create', 'update', 'view', 'delete', 'document'],
+                        'roles' => ['@'],
+                    ],
+                ]
+            ]
         ];
     }
 
@@ -61,6 +78,22 @@ class DogController extends Controller
     }
 
     /**
+     * Finds the Dog model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Dog the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Dog::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+
+    /**
      * Creates a new Dog model.
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
@@ -69,7 +102,7 @@ class DogController extends Controller
     {
         $model = new Dog();
 
-        if(Yii::$app->request->isPost) {
+        if (Yii::$app->request->isPost) {
             $model->load(Yii::$app->request->post());
             $model->breed_id = $this->checkDogBreed($model->breed_title);
             if ($model->validate() && $model->save()) {
@@ -83,6 +116,33 @@ class DogController extends Controller
     }
 
     /**
+     * @param $breed_title
+     * @return bool
+     */
+    protected function checkDogBreed($breed_title)
+    {
+        $breed = DogBreeds::find()->where(['like', 'title', $breed_title])->one();
+
+        if (!$breed) {
+            $breed = new DogBreeds();
+            $breed->title = $breed_title;
+            $breed->status = DogBreeds::STATUS_NEW;
+
+            if (!$breed->save()) {
+
+                return 1;
+            }
+        }
+
+        return $breed->id;
+    }
+
+    public function actionDoc()
+    {
+
+    }
+
+    /**
      * Updates an existing Dog model.
      * If update is successful, the browser will be redirected to the 'view' page.
      * @param integer $id
@@ -93,7 +153,7 @@ class DogController extends Controller
     {
         $model = $this->findModel($id);
 
-        if(Yii::$app->request->isPost) {
+        if (Yii::$app->request->isPost) {
             $model->load(Yii::$app->request->post());
             $model->breed_id = $this->checkDogBreed($model->breed_title);
             if ($model->validate() && $model->save()) {
@@ -104,6 +164,27 @@ class DogController extends Controller
         return $this->render('update', [
             'model' => $model,
         ]);
+    }
+
+    /**
+     * @return Response
+     * @throws Exception
+     * @throws \yii\base\Exception
+     */
+    public function actionDocument()
+    {
+        $phpWord = new PhpWord();
+        $dogs = Dog::find()->all();
+        $section = $phpWord->addSection();
+        /** @var Dog $dog */
+        foreach ($dogs as $dog) {
+            $section->addText('Name: ' . $dog->dog_name . '; owner: ' . $dog->owner . '; breed: ' . $dog->breed->title,
+                ['name' => 'Times New Roman', 'size' => 14]);
+        }
+        $objWriter = IOFactory::createWriter($phpWord);
+        $objWriter->save( 'document.docx');
+        return $this->goBack('/admin');
+
     }
 
     /**
@@ -120,43 +201,5 @@ class DogController extends Controller
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
-    }
-
-    /**
-     * @param $breed_title
-     * @return bool
-     */
-    protected function checkDogBreed($breed_title)
-    {
-        $breed = DogBreeds::find()->where(['like', 'title', $breed_title])->one();
-
-        if(!$breed) {
-            $breed = new DogBreeds();
-            $breed->title = $breed_title;
-            $breed->status = DogBreeds::STATUS_NEW;
-
-            if(!$breed->save()){
-
-                return 1;
-            }
-        }
-
-        return $breed->id;
-    }
-
-    /**
-     * Finds the Dog model based on its primary key value.
-     * If the model is not found, a 404 HTTP exception will be thrown.
-     * @param integer $id
-     * @return Dog the loaded model
-     * @throws NotFoundHttpException if the model cannot be found
-     */
-    protected function findModel($id)
-    {
-        if (($model = Dog::findOne($id)) !== null) {
-            return $model;
-        }
-
-        throw new NotFoundHttpException('The requested page does not exist.');
     }
 }
